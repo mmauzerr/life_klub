944c512b997b2d1d4ead6083861382c765d0b1ac
mmauzerr <mmauzerr@gmail.com>
2016-01-08 00:17:37 +0100

    disabling Gii module

--- config/web.php
+++ config/web.php
@@ -60,10 +60,10 @@ if (YII_ENV_DEV) {
 //        'class' => 'yii\debug\Module',
 //    ];
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
+//    $config['bootstrap'][] = 'gii';
+//    $config['modules']['gii'] = [
+//        'class' => 'yii\gii\Module',
+//    ];
 }
 
 return $config;
