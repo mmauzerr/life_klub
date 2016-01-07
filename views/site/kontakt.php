71763c45f7c3b9f234ac24d7445950c97fe5a5f7
mmauzerr <mmauzerr@gmail.com>
2016-01-08 00:30:14 +0100

    correction

--- views/site/kontakt.php
+++ views/site/kontakt.php
@@ -64,7 +64,7 @@ use dosamigos\google\maps\Map;
 
                     <div class="site-contact">
                         <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-                            <div style="margin-bottom: 20%">
+                            <div style="margin-bottom: 80%">
                                 <div class="alert alert-success col-lg-12 text-center">
                                     Uspešno ste nas kontaktirali.<br />Uskoro ćete dobiti odgovor.
                                 </div>
